1e9984124bb1e9c709bd743a292bcf84b3dfe942
steve-ongera <gadafisteve001@gmail.com>
2026-07-18 03:54:08 +0300

    update frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+
+// Import Bootstrap & Icons
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// Import AOS
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// Import theme styles (your existing main.css)
 import "./styles/main.css";
+
+// Initialize AOS
+AOS.init({
+  duration: 1000,
+  once: true,
+  offset: 100,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
